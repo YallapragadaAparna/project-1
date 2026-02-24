1f513ee3d73e13321d12f34eeee81a874b7accc6
YallapragadaAparna <yaparna2005@gmail.com>
2026-02-24 09:47:25 +0530

    Switch to MySQL connection pool from single connection
    Replaced single connection with a connection pool for better performance and resource management.

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,26 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// });
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
 
 db.connect((err) => {
   if (err) throw err;
   console.log("✅ Connected to MySQL Database");
 });
-
-module.exports = db;
+module.exports = pool;
+//module.exports = db;
